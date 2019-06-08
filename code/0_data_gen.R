23383ba83c49c998e5592ad30d26dd328b014794
Elea McDonnell Feit <eleafeit@gmail.com>
2019-06-08 12:29:25 -0400

    data generation

--- code/0_data_gen.R
+++ code/0_data_gen.R
@@ -2,13 +2,17 @@
 # Elea McDonnell Feit
 # 7 June 2019
 
+# Generates synthetic data for a hypothetical online wine retailer. 
+
+set.seed(89109) 
+
 N <- 123988 # number of users in the test
 
 d <- data.frame(user_id = 1000001:(1000000 + N), 
                 cpgn_id = "1901Email")
 
-# pre-test z's
-# category purchases
+# baseline covariates -----
+# past purchases (by category)
 library(MASS)
 Sigma <- matrix(c(1, 0.3, -0.4, -0.4, 
                   0.3, 1, -0.3, -0.3, 
@@ -23,17 +27,48 @@ d$sav_blanc <- t[,2]
 d$syrah <- t[,3]
 d$cab <- t[,4]
 d$past_purch <- d$chard + d$sav_blanc + d$syrah + d$cab
-d$last_purch = round(exp(rnorm(N, mean=0, sd=5))) 
-d$visits = round(d$past_purch/10 + rnorm(N, mean=5, sd=5))
-d$visits[d$visits < 0] <- 1 * (d$past_purch>0) # at least one visit for purchasers
+# last purchase date
+d$last_purch = round(rexp(N, rate = 1/90))
+# website visits
+d$visits = rpois(N, 5 + d$past_purch/200)
+d$visits[d$visits < 0] <- 0
+d$visits[d$visits == 0 & d$past_purch > 0] <- 1 # at least one visit for purchasers
 
-  
-# test outcomes
-d$open
-d$click
-d$
-group_id = sample(c("A", "B"), N, replace=TRUE)
 
+# RANDOMIZED treatment assignment -----
+# simple random assignment (N known in advance)
+# d$sub_cpgn <- sample(c("email_A", "email_B", "ctrl"), size=N, 
+#                      replace=TRUE, prob=rep(1/3, 3))
+# complete random assignment (N not known in advance)
+d$sub_cpgn <- NA
+rand <- sample(1:N, size=N)
+d$sub_cpgn[rand][1:round(N/3)] <- "email_A"
+d$sub_cpgn[rand][1:round(N/3) + round(N/3)] <- "email_B"
+d$sub_cpgn[is.na(d$sub_cpgn)] <- "ctrl"
+d$sub_cpgn <- factor(d$sub_cpgn, c("email_A", "email_B", "ctrl"))
+
+# test outcomes -----
+p <-  1/(1+exp(-1 - d$past_purch/100 + d$last_purch/60 - 0.5*(d$sub_cpgn=="email_A")))
+d$open <- (runif(N) < p) * 1
+d$open[d$sub_cpgn == "ctrl"] <- 0
+p <- 1/(1+exp(2 - 0.5*(d$sub_cpgn=="email_A") - 0.5*(d$syrah >0)*(d$sub_cpgn=="email_B")))
+d$click <- (runif(N) < p) * 1
+d$click[!(d$open==1)] <- 0
+p <-  1/(1+exp(1 - d$past_purch/300 + d$last_purch/60 - 1*(d$click==1)))
+d$purch <- round(exp(rnorm(N, mean=3.6, sd=1)) * (runif(N) < p), 2)
+
+# inspect and write out -----
 summary(d)
+summary(d$open >= d$click)
+summary(d$past_purch>0 & d$visits==0)
+mosaicplot(table(d$sub_cpgn, d$open))
+prop.test(table(d$sub_cpgn, d$open))
+mosaicplot(table(d$sub_cpgn, d$click))
+prop.test(table(d$sub_cpgn, d$click))
+mosaicplot(table(d$sub_cpgn, d$purch>0))
+prop.test(table(d$sub_cpgn, d$purch==0))
+plot(purch ~ sub_cpgn, data=d)
+by(d, d$sub_cpgn, mean)
 
-write.csv(d, filename="~/repos/ab_test/test_data.csv")
+d <- d[,c(1:2, 10:13, 3:9)]
+write.csv(d, file="~/repos/ab_test/code/test_data.csv")
